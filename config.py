4d89b1dc32d86bd2045b68483608e7c157625d30
chandancv62-mtf <chandan.cv62@gmail.com>
2026-09-14 12:11:06 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+# System Configurations & Parameters
+
+MARKET_START_TIME = "09:15"
+MARKET_END_TIME = "15:15"
+SCHEDULE_CRON_TIME = "15 10 * * 1-5"
+
+RISK_REWARD_RATIOS = [1.0, 2.0]
+
+STOCKS_CSV_PATH = "data/stocks.csv"
+FILTERED_STOCKS_PATH = "data/filtered_stocks.csv"
+ENTRY_SL_SIGNALS_PATH = "data/entry_sl_signals.xlsx"
+WEEKLY_SIGNALS_PATH = "data/weekly_final_trading_signals.xlsx"
+TRADE_TRACKER_PATH = "data/trade_tracker_results.xlsx"
